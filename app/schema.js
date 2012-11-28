39931fa9588b5c90dd38487dc0e60b4add4ee2f5
Seth McLaughlin <s@sethmcl.com>
2012-11-28 09:39:21 -0800

    Fixing some typos

--- app/schema.js
+++ app/schema.js
@@ -1,4 +1,4 @@
-define(['path', 'app/SchemaBuilder'], function(path, SchemaBuilder) {
+define(['path', 'app/schemaBuilder'], function(path, SchemaBuilder) {
   	var schema = new SchemaBuilder(),
   		schemas = schema.parseDirectory('./app/schemas'),
       	schemaSrc = schema.scriptSrc( schemas);
