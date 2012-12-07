0b1dab741d7bc5a6a330f7f7c5c382252ae99b9a
Asa Kusuma <asa.kusuma@gmail.com>
2012-12-06 16:01:52 -0800

    Work on organizing how views manage data updates

--- public/javascripts/views/board.js
+++ public/javascripts/views/board.js
@@ -21,6 +21,16 @@ define([
 			this.el = el;
 			this.data = {};
 			_.bindAll(this);
+
+      //Flag to determine if post render is in process
+      this.isPostRendering = false;
+
+      //Flag to determine whether or not to re-postRender when postRender completes
+      //Used when data is changed while in the middel of postRendering
+      this.refreshOnPostRender = false;
+
+      this.on('postRendered', _.bind(this.onPostRender));
+
 			return [
 				{
 					entityKey: "boards"
@@ -29,21 +39,36 @@ define([
 					entityKey: "cards"
 				}
 			];
+
+      /*
 			this.blockGridsterDraw = false;
 			this.numCards = 0;
 			this.cardLayout = null;
+
+      // Setup data update queue for handling incoming data updates
+      this.dataUpdates = new Eventable();
+      this.dataUpdates.queue = [];
+      this.dataUpdates.queue
+      */
 		},
 		setData: function(query, data) {
 			if(data && data.attributes) {
+        //Board Model
+
 				this.data = data;
+        //Register event for handling updates
 				this.data.on('change', _.bind(this.dataUpdated,this));
+
 				if(this.rendered && typeof window !== 'undefined') {
+          //If markup has already been rendered and we're on the client
+          console.log('MORE DATA!!!');
 					this.postRender();
 				} else {
+          //If we need to render markup
 					dust.render("board", data.attributes, _.bind(this.renderCards,this));
 				}
 			} else if(data && _.isArray(data)) {
-				//load cards
+				//Array of cards
 				this.loadCardTypeahead(data);
 			}
 		},
@@ -52,290 +77,9 @@ define([
 
 
 				/* Typeahead hack */
-
-				  var Typeahead = function (element, options) {
-    this.$element = $(element)
-    this.options = $.extend({}, $.fn.typeahead.defaults, options)
-    this.matcher = this.options.matcher || this.matcher
-    this.sorter = this.options.sorter || this.sorter
-    this.highlighter = this.options.highlighter || this.highlighter
-    this.updater = this.options.updater || this.updater
-    this.$menu = $(this.options.menu).appendTo('body')
-    this.source = this.options.source
-    this.shown = false
-    this.listen()
-  }
-
-  Typeahead.prototype = {
-
-    constructor: Typeahead
-
-  , select: function () {
-      var val = this.$menu.find('.active').attr('data-value')
-      this.$element
-        .val(this.updater(val))
-        .change()
-      return this.hide()
-    }
-
-  , updater: function (item) {
-      return item
-    }
-
-  , show: function () {
-      var pos = $.extend({}, this.$element.offset(), {
-        height: this.$element[0].offsetHeight
-      })
-
-      this.$menu.css({
-        top: pos.top + pos.height
-      , left: pos.left
-      })
-
-      this.$menu.show()
-      this.shown = true
-      return this
-    }
-
-  , hide: function () {
-      this.$menu.hide()
-      this.shown = false
-      return this
-    }
-
-  , lookup: function (event) {
-      var items
-
-      this.query = this.$element.val()
-
-      if (!this.query || this.query.length < this.options.minLength) {
-        return this.shown ? this.hide() : this
-      }
-
-      items = $.isFunction(this.source) ? this.source(this.query, $.proxy(this.process, this)) : this.source
-
-      return items ? this.process(items) : this
-    }
-
-  , process: function (items) {
-      var that = this
-
-      items = $.grep(items, function (item) {
-        return that.matcher(item)
-      })
-
-      items = this.sorter(items)
-
-      if (!items.length) {
-        return this.shown ? this.hide() : this
-      }
-
-      return this.render(items.slice(0, this.options.items)).show()
-    }
-
-  , matcher: function (item) {
-      return ~item.toLowerCase().indexOf(this.query.toLowerCase())
-    }
-
-  , sorter: function (items) {
-      var beginswith = []
-        , caseSensitive = []
-        , caseInsensitive = []
-        , item
-
-      while (item = items.shift()) {
-        if (!item.toLowerCase().indexOf(this.query.toLowerCase())) beginswith.push(item)
-        else if (~item.indexOf(this.query)) caseSensitive.push(item)
-        else caseInsensitive.push(item)
-      }
-
-      return beginswith.concat(caseSensitive, caseInsensitive)
-    }
-
-  , highlighter: function (item) {
-      var query = this.query.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&')
-      return item.replace(new RegExp('(' + query + ')', 'ig'), function ($1, match) {
-        return '<strong>' + match + '</strong>'
-      })
-    }
-
-  , render: function (items) {
-      var that = this
-
-      items = $(items).map(function (i, item) {
-        i = $(that.options.item).attr('data-value', item)
-        i.find('a').html(that.highlighter(item))
-        return i[0]
-      })
-
-      items.first().addClass('active')
-      this.$menu.html(items)
-      return this
-    }
-
-  , next: function (event) {
-      var active = this.$menu.find('.active').removeClass('active')
-        , next = active.next()
-
-      if (!next.length) {
-        next = $(this.$menu.find('li')[0])
-      }
-
-      next.addClass('active')
-    }
-
-  , prev: function (event) {
-      var active = this.$menu.find('.active').removeClass('active')
-        , prev = active.prev()
-
-      if (!prev.length) {
-        prev = this.$menu.find('li').last()
-      }
-
-      prev.addClass('active')
-    }
-
-  , listen: function () {
-      this.$element
-        .on('blur',     $.proxy(this.blur, this))
-        .on('keypress', $.proxy(this.keypress, this))
-        .on('keyup',    $.proxy(this.keyup, this))
-
-      if (this.eventSupported('keydown')) {
-        this.$element.on('keydown', $.proxy(this.keydown, this))
-      }
-
-      this.$menu
-        .on('click', $.proxy(this.click, this))
-        .on('mouseenter', 'li', $.proxy(this.mouseenter, this))
-    }
-
-  , eventSupported: function(eventName) {
-      var isSupported = eventName in this.$element
-      if (!isSupported) {
-        this.$element.setAttribute(eventName, 'return;')
-        isSupported = typeof this.$element[eventName] === 'function'
-      }
-      return isSupported
-    }
-
-  , move: function (e) {
-      if (!this.shown) return
-
-      switch(e.keyCode) {
-        case 9: // tab
-        case 13: // enter
-        case 27: // escape
-          e.preventDefault()
-          break
-
-        case 38: // up arrow
-          e.preventDefault()
-          this.prev()
-          break
-
-        case 40: // down arrow
-          e.preventDefault()
-          this.next()
-          break
-      }
-
-      e.stopPropagation()
-    }
-
-  , keydown: function (e) {
-      this.suppressKeyPressRepeat = !~$.inArray(e.keyCode, [40,38,9,13,27])
-      this.move(e)
-    }
-
-  , keypress: function (e) {
-      if (this.suppressKeyPressRepeat) return
-      this.move(e)
-    }
-
-  , keyup: function (e) {
-      switch(e.keyCode) {
-        case 40: // down arrow
-        case 38: // up arrow
-        case 16: // shift
-        case 17: // ctrl
-        case 18: // alt
-          break
-
-        case 9: // tab
-        case 13: // enter
-          if (!this.shown) return
-          this.select()
-          break
-
-        case 27: // escape
-          if (!this.shown) return
-          this.hide()
-          break
-
-        default:
-          this.lookup()
-      }
-
-      e.stopPropagation()
-      e.preventDefault()
-  }
-
-  , blur: function (e) {
-      var that = this
-      setTimeout(function () { that.hide() }, 150)
-    }
-
-  , click: function (e) {
-      e.stopPropagation()
-      e.preventDefault()
-      this.select()
-    }
-
-  , mouseenter: function (e) {
-      this.$menu.find('.active').removeClass('active')
-      $(e.currentTarget).addClass('active')
-    }
-
-  }
-
-
-  /* TYPEAHEAD PLUGIN DEFINITION
-   * =========================== */
-
-  $.fn.typeahead = function (option) {
-    return this.each(function () {
-      var $this = $(this)
-        , data = $this.data('typeahead')
-        , options = typeof option == 'object' && option
-      if (!data) $this.data('typeahead', (data = new Typeahead(this, options)))
-      if (typeof option == 'string') data[option]()
-    })
-  }
-
-  $.fn.typeahead.defaults = {
-    source: []
-  , items: 8
-  , menu: '<ul class="typeahead dropdown-menu"></ul>'
-  , item: '<li><a href="#"></a></li>'
-  , minLength: 1
-  }
-
-  $.fn.typeahead.Constructor = Typeahead
-
-
- /*   TYPEAHEAD DATA-API
-  * ================== */
-
-  $(document).on('focus.typeahead.data-api', '[data-provide="typeahead"]', function (e) {
-    var $this = $(this)
-    if ($this.data('typeahead')) return
-    e.preventDefault()
-    $this.typeahead($this.data())
-  })
-
+        //Quick fix to avoid the hassle of AMDifying Bootstrap JS plugins
+        var Typeahead=function(e,t){this.$element=$(e);this.options=$.extend({},$.fn.typeahead.defaults,t);this.matcher=this.options.matcher||this.matcher;this.sorter=this.options.sorter||this.sorter;this.highlighter=this.options.highlighter||this.highlighter;this.updater=this.options.updater||this.updater;this.$menu=$(this.options.menu).appendTo("body");this.source=this.options.source;this.shown=false;this.listen()};Typeahead.prototype={constructor:Typeahead,select:function(){var e=this.$menu.find(".active").attr("data-value");this.$element.val(this.updater(e)).change();return this.hide()},updater:function(e){return e},show:function(){var e=$.extend({},this.$element.offset(),{height:this.$element[0].offsetHeight});this.$menu.css({top:e.top+e.height,left:e.left});this.$menu.show();this.shown=true;return this},hide:function(){this.$menu.hide();this.shown=false;return this},lookup:function(e){var t;this.query=this.$element.val();if(!this.query||this.query.length<this.options.minLength){return this.shown?this.hide():this}t=$.isFunction(this.source)?this.source(this.query,$.proxy(this.process,this)):this.source;return t?this.process(t):this},process:function(e){var t=this;e=$.grep(e,function(e){return t.matcher(e)});e=this.sorter(e);if(!e.length){return this.shown?this.hide():this}return this.render(e.slice(0,this.options.items)).show()},matcher:function(e){return~e.toLowerCase().indexOf(this.query.toLowerCase())},sorter:function(e){var t=[],n=[],r=[],i;while(i=e.shift()){if(!i.toLowerCase().indexOf(this.query.toLowerCase()))t.push(i);else if(~i.indexOf(this.query))n.push(i);else r.push(i)}return t.concat(n,r)},highlighter:function(e){var t=this.query.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g,"\\$&");return e.replace(new RegExp("("+t+")","ig"),function(e,t){return"<strong>"+t+"</strong>"})},render:function(e){var t=this;e=$(e).map(function(e,n){e=$(t.options.item).attr("data-value",n);e.find("a").html(t.highlighter(n));return e[0]});e.first().addClass("active");this.$menu.html(e);return this},next:function(e){var t=this.$menu.find(".active").removeClass("active"),n=t.next();if(!n.length){n=$(this.$menu.find("li")[0])}n.addClass("active")},prev:function(e){var t=this.$menu.find(".active").removeClass("active"),n=t.prev();if(!n.length){n=this.$menu.find("li").last()}n.addClass("active")},listen:function(){this.$element.on("blur",$.proxy(this.blur,this)).on("keypress",$.proxy(this.keypress,this)).on("keyup",$.proxy(this.keyup,this));if(this.eventSupported("keydown")){this.$element.on("keydown",$.proxy(this.keydown,this))}this.$menu.on("click",$.proxy(this.click,this)).on("mouseenter","li",$.proxy(this.mouseenter,this))},eventSupported:function(e){var t=e in this.$element;if(!t){this.$element.setAttribute(e,"return;");t=typeof this.$element[e]==="function"}return t},move:function(e){if(!this.shown)return;switch(e.keyCode){case 9:case 13:case 27:e.preventDefault();break;case 38:e.preventDefault();this.prev();break;case 40:e.preventDefault();this.next();break}e.stopPropagation()},keydown:function(e){this.suppressKeyPressRepeat=!~$.inArray(e.keyCode,[40,38,9,13,27]);this.move(e)},keypress:function(e){if(this.suppressKeyPressRepeat)return;this.move(e)},keyup:function(e){switch(e.keyCode){case 40:case 38:case 16:case 17:case 18:break;case 9:case 13:if(!this.shown)return;this.select();break;case 27:if(!this.shown)return;this.hide();break;default:this.lookup()}e.stopPropagation();e.preventDefault()},blur:function(e){var t=this;setTimeout(function(){t.hide()},150)},click:function(e){e.stopPropagation();e.preventDefault();this.select()},mouseenter:function(e){this.$menu.find(".active").removeClass("active");$(e.currentTarget).addClass("active")}};$.fn.typeahead=function(e){return this.each(function(){var t=$(this),n=t.data("typeahead"),r=typeof e=="object"&&e;if(!n)t.data("typeahead",n=new Typeahead(this,r));if(typeof e=="string")n[e]()})};$.fn.typeahead.defaults={source:[],items:8,menu:'<ul class="typeahead dropdown-menu"></ul>',item:'<li><a href="#"></a></li>',minLength:1};$.fn.typeahead.Constructor=Typeahead;$(document).on("focus.typeahead.data-api",'[data-provide="typeahead"]',function(e){var t=$(this);if(t.data("typeahead"))return;e.preventDefault();t.typeahead(t.data())})
 				/* End typeahead hack */
-
 
 
 				$('#add-card-input').typeahead({
@@ -362,14 +106,31 @@ define([
 				},this));
 			}
 		},
+    onPostRender: function() {
+      //When postRender completes, check to see if any data has been updated since
+      //if so, postRender again
+      this.isPostRendering = false;
+      if(this.refreshOnPostRender) {
+        this.refreshOnPostRender = false;
+        this.dataUpdated();
+      }
+    },
 		dataUpdated: function() {
-			var cards = this.data.attributes.cards;
-
-			//Hack for determining if last card is an actual model, or just an id
-			if(cards.length > 0 && cards[cards.length - 1].attributes) {
-				if(this.el) this.updateCards();
-			}
+      if(!this.isPostRendering) {
+        this.isPostRendering = true;
+        this.updateBoard();
+      } else {
+        this.refreshOnPostRender = true;
+      }
 		},
+    updateBoard: function() {
+      console.log('Update board!');
+      var cards = this.data.attributes.cards;
+      //Hack for determining if last card is an actual model, or just an id
+      if(cards.length > 0 && cards[cards.length - 1].attributes) {
+        if(this.el) this.updateCards();
+      }
+    },
 		setDataError: function(query) {
 			this.el.html("<h1>Fatal Data Error</h1>");
 			this.trigger('rendered', this.el.html());
@@ -384,53 +145,67 @@ define([
 
 			if(this.numCards !== cards.length) redraw = true;
 			this.numCards = cards.length;
+      var functions = [];
+
 			for(var i = 0; i < cards.length; i++) {
-  				if(cards[i].attributes) {
-  					cards[i].set('row',row);
-	  				cards[i].set('col',col);
-	  				row = (row+1)%3;
-	  				if(row === 3) col++;
-  				}
-  			}
+				if(cards[i].attributes) {
+					cards[i].set('row',row);
+  				cards[i].set('col',col);
+  				row = (row+1)%3;
+  				if(row === 3) col++;
+				}
+
+        //Create card render function
+        functions.push((function(model, context) {
+          return function(callback) {
+            dust.render("board-card", model.attributes, _.bind(function(err, out) {
+              if(err) throw err;
+              var el = $(out),
+                row = el.attr('data-row'),
+                col = el.attr('data-col');
+              this.gridster.add_widget(out,1,1,col,row);
+
+              //Async parallel callback
+              callback(null, true);
+            }, context));
+          }
+        })(cards[i], this));
+			}
   			
-  			if(layout) {
-  				for(var i = 0; i < cards.length; i++) {
-  					if(layout[cards[i].attributes._id]) {
-  						var id = cards[i].get('_id'),
-  							cardEl = this.ul.find('[data-card-id="'+id+'"]');
-  						if(cardEl.attr('data-row') != layout[id].row || cardEl.attr('data-col') != layout[id].col) {
-  							redraw = true;
-  						}
-  						cards[i].set('row',layout[id].row);
-  						cards[i].set('col',layout[id].col);
-  					}
-  				}
-  			}
-  			if(redraw) {
-  				this.blockGridsterDraw = true;
-  				if(this.gridster.serialize().length !== 0) {
-  					this.gridster.remove_all_widgets((function(cards,view) {
-						return _.once(function() {
-							view.ul.html("");
-							for(var i = 0; i < cards.length; i++) {
-		  						view.addCard(cards[i]);
-		  					}
-		  					view.blockGridsterDraw = false;
-		  					view.numCards = cards.length;
-						});
-					})(cards,this));
-  				} else {
-  					for(var i = 0; i < cards.length; i++) {
-		  				this.addCard(cards[i]);
-		  			}
-		  			this.numCards = cards.length;
-		  			this.blockGridsterDraw = false;
-  				}
-  				
-  			}
+			if(layout) {
+				for(var i = 0; i < cards.length; i++) {
+					if(layout[cards[i].attributes._id]) {
+						var id = cards[i].get('_id'),
+							cardEl = this.ul.find('[data-card-id="'+id+'"]');
+						if(cardEl.attr('data-row') != layout[id].row || cardEl.attr('data-col') != layout[id].col) {
+							redraw = true;
+						}
+						cards[i].set('row',layout[id].row);
+						cards[i].set('col',layout[id].col);
+					}
+				}
+			}
+
+      if(this.gridster.serialize().length !== 0) {
+        //If cards exist, remove them first before re-drawing
+        this.gridster.remove_all_widgets((function(cards,view) {
+          return _.once(function() {
+            view.ul.html("");
+            async.parallel(functions, _.bind(function(err, results) {
+              this.trigger('postRendered');
+            }, this));
+          });
+        })(cards,this));
+      } else {
+        async.parallel(functions, _.bind(function(err, results) {
+          this.trigger('postRendered');
+        }, this));
+      }
+
+      
 		},
 		addCard: function(model) {
-			console.log("ADD CARD: "+ JSON.stringify(model));
+			//console.log("ADD CARD: "+ JSON.stringify(model));
 			dust.render("board-card", model.attributes, _.bind(function(err, out) {
 				if(err) throw err;
 				var el = $(out),
