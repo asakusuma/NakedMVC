a298df6f5e142958ac7b81b7c2216d2a4a8c8175
asakusuma <asa.kusuma@gmail.com>
2012-12-12 14:03:20 -0800

    Get rid of debugging

--- public/javascripts/views/board.js
+++ public/javascripts/views/board.js
@@ -63,7 +63,7 @@ define([
 	loadCardTypeahead: function(data) {
 		if(this.rendered && typeof window !== 'undefined') {
 
-				/* Typeahead hack */
+		/* Typeahead hack */
         //Quick fix to avoid the hassle of AMDifying Bootstrap JS plugins
         var Typeahead=function(e,t){this.$element=$(e);this.options=$.extend({},$.fn.typeahead.defaults,t);this.matcher=this.options.matcher||this.matcher;this.sorter=this.options.sorter||this.sorter;this.highlighter=this.options.highlighter||this.highlighter;this.updater=this.options.updater||this.updater;this.$menu=$(this.options.menu).appendTo("body");this.source=this.options.source;this.shown=false;this.listen()};Typeahead.prototype={constructor:Typeahead,select:function(){var e=this.$menu.find(".active").attr("data-value");this.$element.val(this.updater(e)).change();return this.hide()},updater:function(e){return e},show:function(){var e=$.extend({},this.$element.offset(),{height:this.$element[0].offsetHeight});this.$menu.css({top:e.top+e.height,left:e.left});this.$menu.show();this.shown=true;return this},hide:function(){this.$menu.hide();this.shown=false;return this},lookup:function(e){var t;this.query=this.$element.val();if(!this.query||this.query.length<this.options.minLength){return this.shown?this.hide():this}t=$.isFunction(this.source)?this.source(this.query,$.proxy(this.process,this)):this.source;return t?this.process(t):this},process:function(e){var t=this;e=$.grep(e,function(e){return t.matcher(e)});e=this.sorter(e);if(!e.length){return this.shown?this.hide():this}return this.render(e.slice(0,this.options.items)).show()},matcher:function(e){return~e.toLowerCase().indexOf(this.query.toLowerCase())},sorter:function(e){var t=[],n=[],r=[],i;while(i=e.shift()){if(!i.toLowerCase().indexOf(this.query.toLowerCase()))t.push(i);else if(~i.indexOf(this.query))n.push(i);else r.push(i)}return t.concat(n,r)},highlighter:function(e){var t=this.query.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g,"\\$&");return e.replace(new RegExp("("+t+")","ig"),function(e,t){return"<strong>"+t+"</strong>"})},render:function(e){var t=this;e=$(e).map(function(e,n){e=$(t.options.item).attr("data-value",n);e.find("a").html(t.highlighter(n));return e[0]});e.first().addClass("active");this.$menu.html(e);return this},next:function(e){var t=this.$menu.find(".active").removeClass("active"),n=t.next();if(!n.length){n=$(this.$menu.find("li")[0])}n.addClass("active")},prev:function(e){var t=this.$menu.find(".active").removeClass("active"),n=t.prev();if(!n.length){n=this.$menu.find("li").last()}n.addClass("active")},listen:function(){this.$element.on("blur",$.proxy(this.blur,this)).on("keypress",$.proxy(this.keypress,this)).on("keyup",$.proxy(this.keyup,this));if(this.eventSupported("keydown")){this.$element.on("keydown",$.proxy(this.keydown,this))}this.$menu.on("click",$.proxy(this.click,this)).on("mouseenter","li",$.proxy(this.mouseenter,this))},eventSupported:function(e){var t=e in this.$element;if(!t){this.$element.setAttribute(e,"return;");t=typeof this.$element[e]==="function"}return t},move:function(e){if(!this.shown)return;switch(e.keyCode){case 9:case 13:case 27:e.preventDefault();break;case 38:e.preventDefault();this.prev();break;case 40:e.preventDefault();this.next();break}e.stopPropagation()},keydown:function(e){this.suppressKeyPressRepeat=!~$.inArray(e.keyCode,[40,38,9,13,27]);this.move(e)},keypress:function(e){if(this.suppressKeyPressRepeat)return;this.move(e)},keyup:function(e){switch(e.keyCode){case 40:case 38:case 16:case 17:case 18:break;case 9:case 13:if(!this.shown)return;this.select();break;case 27:if(!this.shown)return;this.hide();break;default:this.lookup()}e.stopPropagation();e.preventDefault()},blur:function(e){var t=this;setTimeout(function(){t.hide()},150)},click:function(e){e.stopPropagation();e.preventDefault();this.select()},mouseenter:function(e){this.$menu.find(".active").removeClass("active");$(e.currentTarget).addClass("active")}};$.fn.typeahead=function(e){return this.each(function(){var t=$(this),n=t.data("typeahead"),r=typeof e=="object"&&e;if(!n)t.data("typeahead",n=new Typeahead(this,r));if(typeof e=="string")n[e]()})};$.fn.typeahead.defaults={source:[],items:8,menu:'<ul class="typeahead dropdown-menu"></ul>',item:'<li><a href="#"></a></li>',minLength:1};$.fn.typeahead.Constructor=Typeahead;$(document).on("focus.typeahead.data-api",'[data-provide="typeahead"]',function(e){var t=$(this);if(t.data("typeahead"))return;e.preventDefault();t.typeahead(t.data())})
         /* End typeahead hack */
@@ -110,7 +110,6 @@ onPostRender: function() {
   	}
   },
   dataUpdatedByMe: function() {
-  	//console.log("I UPDATED MYSELF!!!");
   	if(this.data.attributes.cards.length !== this.numCards) {
   		this.dataUpdated();
   	}
@@ -173,7 +172,6 @@ onPostRender: function() {
       			}
       			cards[i].set('row',layout[id].row);
       			cards[i].set('col',layout[id].col);
-      			console.log(layout[id].row + ' - ' + layout[id].col);
       		}
       	}
       }
