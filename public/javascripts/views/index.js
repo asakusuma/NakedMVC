c61fdb15f9459df0c33ca73be70552e2cd0bf012
asakusuma <asa.kusuma@gmail.com>
2013-01-03 23:43:49 -0600

    Add date to message schema and get rid of old comment

--- public/javascripts/views/index.js
+++ public/javascripts/views/index.js
@@ -10,7 +10,6 @@ define(['lib/underscore', 'dustjs-linkedin', 'base/view','jquery'],function (_,d
 			this.requests.messages.on('resolved', _.bind(this.onMessagesLoaded,this));
 		},
 		onMessagesLoaded: function() {
-			//Junk: ' + this.requests.messages.collection.toJSON() + '
 			this.el = $('<div></div>');
 			this.el.html(JSON.stringify(this.requests.messages.collection.toJSON()));
 			this.render();
