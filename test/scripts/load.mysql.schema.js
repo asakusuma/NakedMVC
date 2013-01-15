c8440d4e4a4aa61a1a61eefd6e542459ccb6fab5
asakusuma <asa.kusuma@gmail.com>
2013-01-14 23:30:03 -0800

    Add basic mysql schema complier

--- test/scripts/load.mysql.schema.js
+++ test/scripts/load.mysql.schema.js
@@ -17,8 +17,40 @@ requirejs(['mysql', 'schema'], function(MySQL, Schemas) {
       password : 'root',
       database: 'rt'
     });
-
-    for(var schema in Schemas) {
+	connection.connect();
+    for(var schema in Schemas.obj) {
     	console.log(schema);
+    	console.log(Schemas.obj[schema]);
+
+    	connection.query('DROP TABLE IF EXISTS '+schema, function(err, rows, fields) {
+		 	if (err) throw err;
+		 	var fields = '_id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(_id) ',
+		 		field,
+		 		map = {
+		 		'text': 'longtext',
+		 		'date': 'int',
+		 		'string': 'varchar(100)',
+		 		'int': 'int'
+		 	};
+		 	for(var name in Schemas.obj[schema].fields) {
+		 		fields += ', ' + name + ' ' + map[Schemas.obj[schema].fields[name].type];
+		 	}
+		 	var query = 'CREATE TABLE '+schema + ' ( '+ fields +' )';
+		 	connection.query(query, function(err, rows, fields) {
+		 		if (err) throw err;
+		  		console.log(arguments);
+		  		connection.end();
+			});
+		});
     }
+    
+/*
+    CREATE TABLE Persons
+(
+FirstName varchar(15),
+LastName varchar(15),
+Age int
+)
+*/
+
 });
